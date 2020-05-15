9f26b0bbedc6d888c48ec5dd217188e1482bce34
ujystrik <ujystrik@mail.ru>
2020-05-15 17:33:57 +0300

    lw7

--- lw7/71/js/PrimeNumber.js
+++ lw7/71/js/PrimeNumber.js
@@ -6,47 +6,46 @@ const ISPRIME = ' is prime number';
 const ISNOTPRIME = ' is not prime number';
 
 function CheckPrimeNumber(digit) {   //Функция возвращает 'No' - если это не простое число, 'Yes' - если это простое число, 'error' - если введенная переменная не числовая
-    if (typeof digit === NUMBER) {  //Проверка на число, цифру
-        if((digit === 0) || (digit === 1)) {
-            return(NO);
-        } else {
-            for (let j = 2; j < digit + 1; j++) {
-                if (digit == j ) {
-                    return(YES); 
-                } else {
-                    if (digit % j === 0) {
-                        return(NO);
-                    }
-                }
-            }
-        }                                                       
+  if (typeof digit === NUMBER) {  //Проверка на число, цифру
+    if((digit === 0) || (digit === 1)) {
+      return(NO);
     } else {
-        return(ERROR);
-    }
+      for (let j = 2; j < digit + 1; j++) {
+        if (digit == j ) {
+          return(YES); 
+        } else {
+          if (digit % j === 0) {
+            return(NO);
+          }
+        }
+      }
+    }                                                       
+  } else {
+    return(ERROR);
+  }
 }
 
 function PrintResultOfChecking(digit){
-    if (CheckPrimeNumber(digit) === YES) {
-        console.log(digit + ISPRIME);
+  if (CheckPrimeNumber(digit) === YES) {
+    console.log(digit + ISPRIME);
+  } else {
+    if (CheckPrimeNumber(digit) === NO) {
+      console.log(digit + ISNOTPRIME);
     } else {
-        if (CheckPrimeNumber(digit) === NO) {
-        console.log(digit + ISNOTPRIME);
-        } else {
-            if (CheckPrimeNumber(digit) === ERROR) {
-                return(ERROR);
-            }
-        }
+      if (CheckPrimeNumber(digit) === ERROR) {
+        return(ERROR);
+      }
     }
-
+  }
 }
 
 function isPrimeNumber(arrORdigit) {
-    if(Array.isArray(arrORdigit)) {      //Проверка на массив
-        let numberofDigit = arrORdigit.length; //определяем количество элементов в массиве
-        for(let i = 0; i < numberofDigit; i++) {
-            PrintResultOfChecking(arrORdigit[i]);
-        }                                      
-    } else {
-        PrintResultOfChecking(arrORdigit);
-    }		
+  if(Array.isArray(arrORdigit)) {      //Проверка на массив
+    let numberofDigit = arrORdigit.length; //определяем количество элементов в массиве
+    for(let i = 0; i < numberofDigit; i++) {
+      PrintResultOfChecking(arrORdigit[i]);
+    }                                      
+  } else {
+    PrintResultOfChecking(arrORdigit);
+  }		
 }
--- lw7/72/js/calc.js
+++ lw7/72/js/calc.js
@@ -9,158 +9,158 @@ const ERRORBRACKET = 'Неверная запись, исправьте коли
 const ERRORWRITE = 'Неверная запись';
 
 function isString(val) {
-    return(typeof val === STRING || val instanceof String);
+  return typeof val === STRING || val instanceof String;
 }
 
 function calc(string){
-	if(isString(string) === false){
- 		return(ERRORSTR);
+  if(isString(string) === false){
+ 	return(ERRORSTR);
+  }
+  if(checkBracket(string) === ERROR){
+    return(ERRORBRACKET);
+  }
+  string = '(' + string + ')';
+  let pastString = '';
+  numberOFbracket = checkBracket(string);
+  pastString = string.substring(firstBracket(string), lastBracket(string) + 1);
+  for(let j = 1; j < numberOFbracket + 1; j++){
+    if(pastCalc(pastString) === ERROR){
+ 	  return(ERRORWRITE);
  	}
- 	if(checkBracket(string) === ERROR){
- 		return(ERRORBRACKET);
- 	}
- 	string = '(' + string + ')';
- 	let pastString = '';
- 	numberOFbracket = checkBracket(string);
  	pastString = string.substring(firstBracket(string), lastBracket(string) + 1);
- 	for(let j = 1; j < numberOFbracket + 1; j++){
- 		if(pastCalc(pastString) === ERROR){
- 			return(ERRORWRITE);
- 		}
- 		pastString = string.substring(firstBracket(string), lastBracket(string) + 1);
- 		string = string.replace(pastString, ' ' + pastCalc(pastString) + ' ');
- 	}
+ 	string = string.replace(pastString, ' ' + pastCalc(pastString) + ' ');
+  }
  	return(readNumber(string, 0));
 }
 
 function checkBracket(string){
-	var numberOFsymbol = string.length;
-	var numberOFfirstSymbol = 0;
-	var numberOFsecondSymbol = 0;
-	for(let i = 0; i != numberOFsymbol; i++){
-		if(string[i] === '('){
-			numberOFfirstSymbol++;
-		}
-		if(string[i] === ')'){
-			numberOFsecondSymbol++;
-		}
+  var numberOFsymbol = string.length;
+  var numberOFfirstSymbol = 0;
+  var numberOFsecondSymbol = 0;
+  for(let i = 0; i != numberOFsymbol; i++){
+    if(string[i] === '('){
+	numberOFfirstSymbol++;
 	}
-	if(numberOFfirstSymbol === numberOFsecondSymbol){
-		return(numberOFfirstSymbol);
-	} else {
-		return(ERROR);
+	if(string[i] === ')'){
+	  numberOFsecondSymbol++;
 	}
+  }
+  if(numberOFfirstSymbol === numberOFsecondSymbol){
+	return(numberOFfirstSymbol);
+  } else {
+	return(ERROR);
+  }
 }
 
 function firstBracket(string){
-	var backetPosition = undefined;
-	var numberOFsymbol = string.length;
-	for(i = 0; i < numberOFsymbol; i++){
-		if(string[i] === '('){
-			backetPosition = i;
-		}
+  var backetPosition = undefined;
+  var numberOFsymbol = string.length;
+  for(i = 0; i < numberOFsymbol; i++){
+	if(string[i] === '('){
+	  backetPosition = i;
 	}
-	return(backetPosition);
+  }
+  return(backetPosition);
 }
 
 function lastBracket(string){
-	var backetPosition = undefined;
-	var i = firstBracket(string);
-	while(string[i] !== ')'){
-		i++;
-	}
-	return(i);
+  var backetPosition = undefined;
+  var i = firstBracket(string);
+  while(string[i] !== ')'){
+	i++;
+  }
+  return(i);
 }
 
 function passNumber(string, position){
-	if(string[position] === '-'){
-		position++;
-	}
-	position = passSpace(string, position);
-	while(STRINGNUMBER.includes(string[position])){
-		position++;
-	}
-	return(position);
+  if(string[position] === '-'){
+	position++;
+  }
+  position = passSpace(string, position);
+  while(STRINGNUMBER.includes(string[position])){
+	position++;
+  }
+  return(position);
 }
 
 function passSpace(string, position){
-	while(string[position] === ' '){
-		position++;
-	}
-	return(position);
+  while(string[position] === ' '){
+	position++;
+  }
+  return(position);
 }
 
 function subString(string, position){
+  position = passSpace(string, position);
+  let numberString = undefined;
+  let plus = YES;
+  if(string[position] === '-'){
+	position++;
 	position = passSpace(string, position);
-	let numberString = undefined;
-	let plus = YES;
-	if(string[position] === '-'){
-		position++;
-		position = passSpace(string, position);
-		plus = NO;
-	}
-	if(STRINGNUMBER.includes(string[position])){
-		numberString = '';
-	}
-	while(STRINGNUMBER.includes(string[position])){
-		numberString = numberString + string[position];
-		position++;
-	}
-	if(plus === NO){
-		return('-' + numberString);
-	}
-	return(numberString);
+	plus = NO;
+  }
+  if(STRINGNUMBER.includes(string[position])){
+	numberString = '';
+  }
+  while(STRINGNUMBER.includes(string[position])){
+	numberString = numberString + string[position];
+	position++;
+  }
+  if(plus === NO){
+	return('-' + numberString);
+  }
+  return(numberString);
 }
 
 function readNumber(string, position){
-	return(parseInt(subString(string, position), 10));
+  return(parseInt(subString(string, position), 10));
 }
 
 function answer(sign, firstNumber, secondNumber){
-	switch (sign){
-		case '-':
-            return(firstNumber - secondNumber);
-		break;
-		case '+':
-		    return(firstNumber + secondNumber);
-		break;
-		case '*':
-		    return(firstNumber * secondNumber);
-		break;
-		case '/':
-		    return(firstNumber / secondNumber);
-		break;
-	}
+  switch (sign){
+	case '-':
+      return(firstNumber - secondNumber);
+	break;
+	case '+':
+      return(firstNumber + secondNumber);
+	break;
+	case '*':
+	  return(firstNumber * secondNumber);
+	break;
+	case '/':
+	  return(firstNumber / secondNumber);
+	break;
+  }
 }
 
 function pastCalc(string){
-	let position = 1;
-	position = passSpace(string, position);
-	if(STRINGSIGN.includes(string[position])){  //найден знак
-		var sign = string[position];            //sign-знак
-		position++;
-	} else {
-		return(ERROR);
-	}
-	position = passSpace(string, position);
-	if(isNaN(subString(string, position)) === false){
-		var firstNumber = readNumber(string, position);
-	} else {
-		return(ERROR);
-	}
-	position = passNumber(string, position);
-	if(isNaN(subString(string, position)) === false){
-		var secondNumber = readNumber(string, position);
-	} else {
-		return(ERROR);
-	}
-	position = passSpace(string, position);
-	position = passNumber(string, position);
-	position = passSpace(string, position);
-	var answerFull = answer(sign, firstNumber, secondNumber);
-	if(string[position] === ')'){
-		return(answerFull);
-	} else {
-		return(ERROR);
-	}
+  let position = 1;
+  position = passSpace(string, position);
+  if(STRINGSIGN.includes(string[position])){  //найден знак
+	var sign = string[position];            //sign-знак
+	position++;
+  } else {
+	return(ERROR);
+  }
+  position = passSpace(string, position);
+  if(isNaN(subString(string, position)) === false){
+	var firstNumber = readNumber(string, position);
+  } else {
+	return(ERROR);
+  }
+  position = passNumber(string, position);
+  if(isNaN(subString(string, position)) === false){
+	var secondNumber = readNumber(string, position);
+  } else {
+	return(ERROR);
+  }
+  position = passSpace(string, position);
+  position = passNumber(string, position);
+  position = passSpace(string, position);
+  var answerFull = answer(sign, firstNumber, secondNumber);
+  if(string[position] === ')'){
+	return(answerFull);
+  } else {
+	return(ERROR);
+  }
 }

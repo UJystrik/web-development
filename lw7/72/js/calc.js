0e0257a1e838045e6ca3e61c1fba6489de04eaa2
ujystrik <ujystrik@mail.ru>
2020-05-16 18:53:13 +0300

    lw7

--- lw7/72/js/calc.js
+++ lw7/72/js/calc.js
@@ -12,19 +12,19 @@ function isString(val) {
     return typeof val === STRING || val instanceof String;
 }
 
-function calc(string){
-    if (isString(string) === false){
+function calc(string) {
+    if (isString(string) === false) {
         return ERRORSTR;
     }
-    if (checkBracket(string) === ERROR){
+    if (checkBracket(string) === ERROR) {
         return ERRORBRACKET;
     }
     string = '(' + string + ')';
     let pastString = '';
     numberOFbracket = checkBracket(string);
     pastString = string.substring(firstBracket(string), lastBracket(string) + 1);
-    for (let j = 1; j < numberOFbracket + 1; j++){
-        if (pastCalc(pastString) === ERROR){
+    for (let j = 1; j < numberOFbracket + 1; j++) {
+        if (pastCalc(pastString) === ERROR) {
  	    return ERRORWRITE;
  	}
  	pastString = string.substring(firstBracket(string), lastBracket(string) + 1);
@@ -33,90 +33,90 @@ function calc(string){
  	return readNumber(string, 0);
 }
 
-function checkBracket(string){
+function checkBracket(string) {
     var numberOFsymbol = string.length;
     var numberOFfirstSymbol = 0;
     var numberOFsecondSymbol = 0;
-    for (let i = 0; i != numberOFsymbol; i++){
-        if (string[i] === '('){
+    for (let i = 0; i != numberOFsymbol; i++) {
+        if (string[i] === '(') {
             numberOFfirstSymbol++;
 	}
-        if (string[i] === ')'){
+        if (string[i] === ')') {
 	    numberOFsecondSymbol++;
 	}
     }
-    if (numberOFfirstSymbol === numberOFsecondSymbol){
+    if (numberOFfirstSymbol === numberOFsecondSymbol) {
         return numberOFfirstSymbol;
     } else {
         return ERROR;
     }
 }
 
-function firstBracket(string){
+function firstBracket(string) {
     var backetPosition = undefined;
     var numberOFsymbol = string.length;
-    for (i = 0; i < numberOFsymbol; i++){
-        if (string[i] === '('){
+    for (i = 0; i < numberOFsymbol; i++) {
+        if (string[i] === '(') {
 	    backetPosition = i;
 	}
     }
     return backetPosition;
 }
 
-function lastBracket(string){
+function lastBracket(string) {
     var backetPosition = undefined;
     var i = firstBracket(string);
-    while (string[i] !== ')'){
+    while (string[i] !== ')') {
         i++;
     }
     return i;
 }
 
-function passNumber(string, position){
-    if (string[position] === '-'){
+function passNumber(string, position) {
+    if (string[position] === '-') {
         position++;
     }
     position = passSpace(string, position);
-    while (STRINGNUMBER.includes(string[position])){
+    while (STRINGNUMBER.includes(string[position])) {
         position++;
     }  
     return position;
 }
 
-function passSpace(string, position){
-    while (string[position] === ' '){
-        position++;
+function passSpace(string, position) {
+    while (string[position] === ' ') {
+        position++;                  
     }
     return position;
 }
 
-function subString(string, position){
+function subString(string, position) {
     position = passSpace(string, position);
     let numberString = undefined;
     let plus = YES;
-    if (string[position] === '-'){
+    if (string[position] === '-') {
         position++;
 	position = passSpace(string, position);
 	plus = NO;
     }
-    if (STRINGNUMBER.includes(string[position])){
+    if (STRINGNUMBER.includes(string[position])) {
         numberString = '';
     }
-    while (STRINGNUMBER.includes(string[position])){
+    while (STRINGNUMBER.includes(string[position])) {
         numberString = numberString + string[position];
 	position++;
     }
-    if (plus === NO){
+    if (plus === NO) {
         return '-' + numberString;
     }
     return numberString;
 }
 
-function readNumber(string, position){
+function readNumber(string, position) {
     return parseInt(subString(string, position), 10);
 }
 
-function answer(sign, firstNumber, secondNumber){
+function answer(sign, firstNumber, secondNumber) {
     switch (sign){
         case '-':
             return firstNumber - secondNumber;
@@ -133,23 +133,23 @@ function answer(sign, firstNumber, secondNumber){
     }
 }
 
-function pastCalc(string){
+function pastCalc(string) {
     let position = 1;
     position = passSpace(string, position);
-    if (STRINGSIGN.includes(string[position])){  //найден знак
+    if (STRINGSIGN.includes(string[position])) {  //найден знак
 	var sign = string[position];            //sign-знак
 	position++;
     } else {
   	return ERROR;
     }
     position = passSpace(string, position);
-    if (isNaN(subString(string, position)) === false){
+    if (isNaN(subString(string, position)) === false) {
 	var firstNumber = readNumber(string, position);
     } else {
 	return ERROR;
     }
     position = passNumber(string, position);
-    if (isNaN(subString(string, position)) === false){
+    if (isNaN(subString(string, position)) === false) {
 	var secondNumber = readNumber(string, position);
     } else {
 	return ERROR;
@@ -158,7 +158,7 @@ function pastCalc(string){
     position = passNumber(string, position);
     position = passSpace(string, position);
     var answerFull = answer(sign, firstNumber, secondNumber);
-    if (string[position] === ')'){
+    if (string[position] === ')') {
 	return answerFull;
     } else {
 	return ERROR;
